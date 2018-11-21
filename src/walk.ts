c5736f17793b1edfd9b52043f1b8a8ab99ce1ca4
nampdn <nampdn@protonmail.com>
2018-11-21 16:52:53 +0700

    fix(deps): cicle dependencies between walk and index

--- src/walk.ts
+++ src/walk.ts
@@ -1,7 +1,8 @@
 import {join} from 'path'
-import * as R from 'ramda'
-import {statAsync, readdirAsync} from './index'
+import {map, forEach} from 'ramda'
+import {statAsync, readdirAsync} from './builtins'
 
+// Walk
 const getPathInfo = async (path: string) => {
   const stats = await statAsync(path)
   return {path, isDir: stats.isDirectory()}
@@ -9,7 +10,7 @@ const getPathInfo = async (path: string) => {
 
 const getFilesInDir = async (path: string) => {
   const files = await readdirAsync(path)
-  return R.map((file: any) => join(path, file), files)
+  return map((file: any) => join(path, file), files)
 }
 
 /**
@@ -22,7 +23,7 @@ export const walk = async (path: string): Promise<string[]> => {
   const contents = await getFilesInDir(path)
   const pathInfoArray = await Promise.all(contents.map(getPathInfo))
 
-  R.forEach(({isDir, path}: any) => (isDir ? folders : files).push(path), pathInfoArray)
+  forEach(({isDir, path}: any) => (isDir ? folders : files).push(path), pathInfoArray)
 
   const result = await Promise.all(folders.map(walk))
   return files.concat(...result)
